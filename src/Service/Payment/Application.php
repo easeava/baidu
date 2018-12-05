093ccae4f6bc1ab98789eb385482860c6608757b
easeava <tthd@163.com>
2018-12-05 22:48:49 +0800

    payment service base

---
+++ src/Service/Payment/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * EaseBaidu - A PHP Framework For Baidu SDK
+ *
+ * @package  Easeava/baidu
+ * @author   Easeava <tthd@163.com>
+ */
+
+namespace EaseBaidu\Service\Payment;
+
+use EaseBaidu\Kernel\Container;
+
+class Application extends Container
+{
+    protected $providers = [
+
+    ];
+}
