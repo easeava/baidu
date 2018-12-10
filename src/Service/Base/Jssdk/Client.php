43600a47dc5fd01bd30d10a2d9d06be730a67494
太年轻 <tthd@163.com>
2018-12-10 18:16:44 +0800

    add base jssdk service

---
+++ src/Service/Base/Jssdk/Client.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Easeava package.
+ *
+ * (c) Easeava <tthd@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EaseBaidu\Service\Base\Jssdk;
+
+use EaseBaidu\Kernel\BaseClient;
+use EaseBaidu\Kernel\Traits\InteractsWithCache;
+
+class Client extends BaseClient
+{
+    use InteractsWithCache;
+}
