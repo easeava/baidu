d48fb831b90f067491e3db6a960a2c1fda665c11
太年轻 <tthd@163.com>
2018-12-14 09:25:56 +0800

    move swan to smartprogram

---
+++ src/Service/SmartProgram/Application.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Easeava package.
+ *
+ * (c) Easeava <tthd@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EaseBaidu\Service\SmartProgram;
+
+use EaseBaidu\Kernel\Container;
+
+class Application extends Container
+{
+    protected $providers = [
+
+    ];
+}
